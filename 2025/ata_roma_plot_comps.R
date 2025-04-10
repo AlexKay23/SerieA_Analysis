3359b204247e63ea9823b63bc641403c066e2414
AlexKay23 <ak23alex@gmail.com>
2025-04-10 16:31:13 -0400

    updates

--- 2025/ata_roma_plot_comps.R
+++ 2025/ata_roma_plot_comps.R
@@ -1,17 +1,35 @@
 source("2025/SerieA Team Match Ratings.R")
 
 library(gghighlight)
+library(cowplot)
 
-ggplot(nn, aes(x=datetime,y=xg,color = teamHelper))+
+xg_comp1 <- ggplot(nn, aes(x=datetime,y=xg,color = teamHelper))+
   geom_line(size=3)+
   geom_point(size=5)+
   gghighlight(teamHelper %in% c("Atalanta","Roma"))+
-  theme_bw()
+  scale_color_manual(values = c("Atalanta" = "#1e71b8", "Roma" = "#8e1f2f"))+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  labs(title = "Expected Goals by Week 2024/2025",
+       y = "xG ",
+       x = "Date")
 
 
-ggplot(nn, aes(x=datetime,y=goals,color = teamHelper))+
+xg_comp2 <- ggplot(nn, aes(x=datetime,y=goals,color = teamHelper))+
   geom_line(size=3)+
   geom_point(size=5)+
   gghighlight(teamHelper %in% c("Atalanta","Roma"))+
-  theme_bw()
+  scale_color_manual(values = c("Atalanta" = "#1e71b8", "Roma" = "#8e1f2f"))+
+  theme_bw()+
+  theme(panel.grid = element_blank())+
+  labs(title = "Goals by Week 2024/2025",
+       y = "# of Goals ",
+       x = "Date",
+       caption = "Source: Understat.com")
 
+
+cbind_xg_g_plot <- plot_grid(xg_comp1,
+          xg_comp2,
+          ncol = 1)
+
+ggsave(plot = cbind_xg_g_plot,filename = "images/ata_roma_xg_g_combined.png")
